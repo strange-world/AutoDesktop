0de6986a917ad6b088d11be5c94ec2c4fc3f82d8
Eric Lawrence <134732988+strange-world@users.noreply.github.com>
2023-05-28 16:20:23 -0500

    Update app.java
    Multi page + application opening functionality.

--- app.java
+++ app.java
@@ -1,39 +1,48 @@
 import java.awt.Desktop;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
-import java.awt.Rectangle;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 
 public class app {
     public static void main(String[] args) {
-        String htmlFilePath = "C:/Java/AutoDesktop/Auto/new_window.html";
+        String[] paths = {
+            "C:/Program Files/NVIDIA Corporation/NVIDIA GeForce Experience/NVIDIA GeForce Experience.exe",
+            "C:/Java/AutoDesktop/Auto/new_window.html",
+            "https://www.epicgames.com/"
+        };
+
         int displayIndex = 0; // Index of the display to open the window on
 
         try {
-            File file = new File(htmlFilePath);
-            Desktop desktop = Desktop.getDesktop();
-
-            // Get the GraphicsDevice for the specified display index
             GraphicsDevice[] devices = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
             if (displayIndex >= 0 && displayIndex < devices.length) {
-                GraphicsDevice targetDevice = devices[displayIndex];
 
-                // Get the bounds of the target display
-                Rectangle displayBounds = targetDevice.getDefaultConfiguration().getBounds();
+                Desktop desktop = Desktop.getDesktop();
 
-                // Set the position and size of the window based on the target display bounds
-                String options = String.format("width=%d,height=%d,x=%d,y=%d",
-                        500, 500, displayBounds.x, displayBounds.y);
+                for (String path : paths) {
+                    if (path.equals("C:/Java/AutoDesktop/Auto/new_window.html")) {
+                        // Skip opening the new_window.html file
+                        continue;
+                    }
 
-                // Open the HTML file on the target display
-                desktop.open(file);
-                Thread.sleep(1000); // Wait for the window to open
-                desktop.browse(file.toURI().resolve(options));
+                    if (path.startsWith("http")) {
+                        desktop.browse(new URI(path));
+                    } else {
+                        File file = new File(path);
+                        if (file.exists()) {
+                            desktop.open(file);
+                            Thread.sleep(1000); // Wait for the application/window to open
+                        } else {
+                            System.err.println("File does not exist: " + file.getAbsolutePath());
+                        }
+                    }
+                }
             } else {
                 System.err.println("Invalid display index: " + displayIndex);
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException | InterruptedException | java.net.URISyntaxException e) {
             e.printStackTrace();
         }
     }
